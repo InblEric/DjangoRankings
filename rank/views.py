d394ce423a53fbfd1e339c3320ba92174e00a903
Eric Wehrmeister <ericwehrmeister@erics-mbp.attlocal.net>
2014-10-23 17:01:39 -0500

    Has home site print out player list

--- rank/views.py
+++ rank/views.py
@@ -6,6 +6,9 @@ from django.http import HttpResponse
 from rank.models import Player,Matchup
 
 def index(request):
-    player = Player.objects.get(pk=1)
+    plist = []
+    for i in Player.objects.all():
+        pass
+        plist.append(str(i.__unicode__()))
     
-    return HttpResponse("Hello, world. You're at the rankings index." + " Players: " +str(player) )
+    return HttpResponse("Hello, world. You're at the rankings index." + " Players: " +str(plist) )
