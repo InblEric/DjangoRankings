22ebfcffc99a3c32f0da5923edbdd02dc46c1777
Eric Wehrmeister <ericwehrmeister@erics-mbp.attlocal.net>
2014-10-23 15:43:08 -0500

    More work on models and created some db entries

--- rank/models.py
+++ rank/models.py
@@ -4,6 +4,9 @@ class Player(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     position = models.CharField(max_length=100)
+    
+    def __unicode__(self):
+        return self.first_name + " " + self.last_name + ", " + self.position
 
 class Matchup(models.Model):
     player1 = models.ForeignKey(Player, related_name='player_1')
@@ -11,3 +14,17 @@ class Matchup(models.Model):
     position = models.CharField(max_length=100) #QB/RB/WR/TE/FLEX/DST/K
     p1Votes = models.IntegerField()
     p2Votes = models.IntegerField()
+    
+    def matchupLeader(self):
+        if(self.p1Votes > self.p2Votes):
+            return self.player1.__unicode__() + " wins by " + str(self.p1Votes-self.p2Votes) + " votes against " + self.player2.__unicode__()
+        elif(self.p2Votes > self.p1Votes):
+            return self.player2.__unicode__() + " wins by " + str(self.p2Votes-self.p1Votes) + " votes against " + self.player1.__unicode__()
+        else:
+            return "Tie between " + self.player1.__unicode__() + " and " + self.player2.__unicode__()
+    
+    def __unicode__(self):
+        player1name = self.player1.__unicode__()
+        player2name = self.player2.__unicode__()
+        
+        return player1name + " vs " + player2name
