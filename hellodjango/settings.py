fe76852f1173d7ddf00950ea1e4bf52e81f90d33
Eric Wehrmeister <ericwehrmeister@erics-mbp.attlocal.net>
2014-10-24 16:53:26 -0500

    Fixed CSS junk

--- hellodjango/settings.py
+++ hellodjango/settings.py
@@ -107,8 +107,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-"""
+
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'bootstrap'),
+    os.path.join(BASE_DIR, 'michael'),
 )
-"""
