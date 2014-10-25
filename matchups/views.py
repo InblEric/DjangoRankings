65c925540198b88e4bb8525aa457f9ded8ae0b17
Eric Wehrmeister <ericwehrmeister@erics-mbp.attlocal.net>
2014-10-25 15:29:07 -0500

    Added navigation through matchups

--- matchups/views.py
+++ matchups/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.template import RequestContext, loader
+import logging
 
 # Create your views here.
 
@@ -13,7 +14,14 @@ def mindex(request):
     return HttpResponse(template.render(context))
     
 def get(request, id):
+    names = ["none","none"]
+    mList = list(Matchup.objects.all())
+    try:
+        matchup = mList[int(id)-1]
+    except:
+        return HttpResponse("invalid matchup")    
+    names[0] = (str(matchup.player1.first_name) + " " + str(matchup.player1.last_name))
+    names[1] = (str(matchup.player2.first_name) + " " + str(matchup.player2.last_name))        
     template = loader.get_template('matchups/specific.html')
-    context = RequestContext(request, {'id':id})
-    #return HttpResponse("id = " + str(id))    
+    context = RequestContext(request, {'id':id, 'names':names,})
     return HttpResponse(template.render(context))
